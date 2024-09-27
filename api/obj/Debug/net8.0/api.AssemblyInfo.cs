ee1ada8623aef6c2e59b7e0dfce143db07bf9379
Vincent Pham <vincent.pham@hcpcaregivers.com>
2024-09-27 09:32:42 -0500

    Finished 13. Comment Get + Include()

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53b2be200aa9b5c5ed009afff8d0db656de84154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a58e6a0778538d215f8e9e5d017e6f2a14ca0db")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
