196894aa118809c27cef5f746f75a3ea56557eb7
Vincent Pham <vincent.pham@hcpcaregivers.com>
2024-09-27 14:37:48 -0500

    Finished 18. Filtering

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9014ffa45b18f873aa74530dc51a245d22ab8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bed1adfd4fb481725c0bacfa4eea746eed584b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
