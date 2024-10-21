48e57e1b7a35e68e1b9f8f0e3fcc977d1e3ad8f7
Vincent Pham <vincent.pham@hcpcaregivers.com>
2024-10-21 14:54:07 -0500

    Finished 27. Portfolio CREATE

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac0dfd837f091157ec5cdb35b0734de445c6f04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2ac62a3eca88c1e869ffa8f9de36b3efc588d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
