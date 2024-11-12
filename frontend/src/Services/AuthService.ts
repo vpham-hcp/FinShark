cef268a4df9859410490b867630fafe328e2f83f
Vincent Pham <vincent.pham@hcpcaregivers.com>
2024-11-12 09:12:16 -0600

    Finished 33. Services

---
+++ frontend/src/Services/AuthService.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { handleError } from '../Helpers/ErrorHandler';
+import { UserProfileToken } from '../Models/User';
+
+const api = 'http://localhost:5254/api';
+
+export const loginAPI = async (username: string, password: string) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + 'account/login', {
+      username: username,
+      password: password,
+    });
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const registerAPI = async (
+  email: string,
+  username: string,
+  password: string
+) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + 'account/register', {
+      email: email,
+      username: username,
+      password: password,
+    });
+  } catch (error) {
+    handleError(error);
+  }
+};
